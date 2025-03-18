a3d1594dc993dac2ec0f5d9c61ddd207197fc28f
dipakgawade37 <dipakgawade37@gmail.com>
2025-03-18 12:16:01 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,22 +1,5 @@
-resource "azurerm_resource_group" "rg-dipak" {
-    name      = var.resource_group
-    location  = var.location
-
-}
-
-resource "azurerm_virtual_network" "vnets" {
-  count               = length(var.env1)
-  name                = var.vnet[count.index]
-  location            = var.location
-  address_space       = [var.address[count.index]]
-  resource_group_name = var.resource_group
-}
-
-resource "azurerm_resource_group" "rg" {
-    for_each = (var.env1)
-    name     = each.key
-    location = var.location
-    tags = {
-        Environment = each.value
-    }
+resource "azurerm_resource_group" "RG-dipak1" {
+    location = "eastus2"
+    name     = "RG-dipak1"
+  
 }
--- variables.tf
+++ variables.tf
@@ -1,44 +1,19 @@
-variable "location" {
-  description = "test"
-  type        = string
-  default     = "West Europe"
+variable "client_id" {
+  type = string
+  
+}
 
+variable "client_secret" {
+  type = string
+  
 }
 
 variable "sub_id" {
-
-  default     = "628e279f-926f-43cc-bc30-24777c6b91ef"
-  description = "sub ID"
-  type        = string
-
+  type = string
+  
 }
 
-variable "resource_group" {
-
-  default = "RG-Dipak"
-  type    = string
-
-}
-
-variable "vnet" {
-
-  type        = list(string)
-  default     = ["vnet1", "vnet2"]
-  description = "vnet names"
-
-}
-
-variable "address" {
-  type        = list(string)
-  default     = ["10.10.2.0/24", "10.10.3.0/24"]
-  description = "adresses"
-}
-
-variable "env1" {
-  type = map(string)
-  default = {
-    "prd-dipak" = "production"
-    "dev-dipak" = "develpment"
-  }
-  description = "envs"
+variable "tenant_id" {
+  type = string 
+  
 }

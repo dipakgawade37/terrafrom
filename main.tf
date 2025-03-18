2a658772aea0f3b39226a83af8226f720f1a5dae
Gawade <Dipak.Gawade@Saint-gobain.com>
2025-03-18 12:49:24 +0530

    added_vnet

--- main.tf
+++ main.tf
@@ -3,3 +3,11 @@ resource "azurerm_resource_group" "RG-dipak1" {
     name     = "RG-dipak1"
   
 }
+
+resource "azurerm_virtual_network" "vnet1_dipak1" {
+    name                = "vnet1"
+    location            = azurerm_resource_group.RG-dipak1.location
+    resource_group_name = azurerm_resource_group.RG-dipak1.name
+    address_space       = ["10.10.0.0/16"]
+  
+}

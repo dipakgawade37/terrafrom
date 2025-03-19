c96cea198db8c2ae1035c711ba372e106a9d4140
dipakgawade37 <dipakgawade37@gmail.com>
2025-03-19 14:37:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "azurerm_virtual_network" "vnet1_dipak1" {
 }
 
 resource "azurerm_virtual_network" "vnet1_dipak2" {
-  name                = "vnet4"
+  name                = "vnet3"
   location            = azurerm_resource_group.RG-dipak1.location
   resource_group_name = azurerm_resource_group.RG-dipak1.name
   address_space       = ["10.10.1.0/24"]

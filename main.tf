14063db7215c34523f6a3ad3d87d8535830edf57
dipakgawade37 <dipakgawade37@gmail.com>
2025-08-08 10:20:33 +0530

    code QL vulenrabiltiy Update main.tf

--- main.tf
+++ main.tf
@@ -31,3 +31,27 @@ resource "azurerm_virtual_network" "vnet1_dipak2" {
     Owner       = "Dipak"
   }
 }
+
+# 🚨 Intentional Vulnerability: Open inbound access to all IPs
+resource "azurerm_network_security_group" "nsg_public" {
+  name                = "nsg-public"
+  location            = azurerm_resource_group.RG-dipak1.location
+  resource_group_name = azurerm_resource_group.RG-dipak1.name
+
+  security_rule {
+    name                       = "Allow-All-Inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "0.0.0.0/0"   # ❌ Open to the world
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    Environment = "Development"
+    Owner       = "Dipak"
+  }
+}
